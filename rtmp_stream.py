13532d159e4784b2baaef999f876d01b200757d4
ThePotato456 <thepotato456@yahoo.com>
2023-09-20 22:19:33 -0500

    updated, works on windows

--- rtmp_stream.py
+++ rtmp_stream.py
@@ -1,26 +1,32 @@
 #!./.venv/bin/python
 
-import ffmpeg, dotenv, subprocess, io, os, argparse
+import ffmpeg, dotenv, subprocess, io, os, argparse, json, time
 
 from colorama import init, just_fix_windows_console, Fore, Style
 printi = lambda *a, **kw: print(f' {Style.BRIGHT}[{Fore.CYAN}i{Fore.RESET}]{Style.RESET_ALL}', *a, **kw)
 printe = lambda *a, **kw: print(f' {Style.BRIGHT}[{Fore.LIGHTRED_EX}-{Fore.RESET}]', *a, **kw)
 prints = lambda *a, **kw: print(f' {Style.BRIGHT}[{Fore.LIGHTGREEN_EX}+{Fore.RESET}]', *a, **kw)
 
+def load_playlist():
+    queue = []
+    if os.path.exists('./media'):
+        movie_list = os.listdir('./media')
+        
+        for movie in movie_list:
+            movie_path = os.path.abspath(os.path.join('./media', movie))
+            movie_info = { 'name': movie, 'path': movie_path }
+            
+            queue.append(movie_info)
+        return queue
+    else:
+        return False
 
-def stream_mp4_to_rtmp(input_file, rtmp_url):
-    try:
-        ffmpeg.input(input_file, ).output(rtmp_url, format='flv', vcodec='libx264').run(capture_stdout=True, capture_stderr=True)
-    except ffmpeg.Error as e:
-        print(f"FFmpeg error: {e.stderr.decode('utf-8')}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-import subprocess
-
-def stream_to_owncast(input_file, rtmp_server_url, stream_key, video_bitrate=1800, audio_bitrate=48000):
+def stream_to_owncast(input_file, rtmp_server_url, stream_key, video_bitrate=1800, audio_bitrate=86000):
     ffmpeg_cmd = [
-        'ffmpeg',
+        'ffmpeg.exe',
+        '-hide_banner',
+        '-loglevel',
+        'error',
         '-re',
         '-i', input_file,
         '-c:v', 'libx264',
@@ -48,26 +54,36 @@ if __name__ == "__main__":
     parser.add_argument(
         'input_file',
         help=f"Path of the file to be streamed.",
+        default=None,
+        nargs='?',
         type=argparse.FileType('r')
     )
     args = parser.parse_args()
     
-    if not args.input_file:
-        printe(F"No file specified!")
-        exit()
-    
-    input_file: io.TextIOWrapper = args.input_file
-    file_name = input_file.name
-    
-    if input_file is None:
-        printe('[main] no file specified')
-        exit(2)
+    queue = []
+    if args.input_file is None:
+        printe(F"No file specified, defaulting to playlist in ./media/")
+        queue = load_playlist()
+        printi(f"Playlist with {len(queue)} movies loaded.")
+        
+        for movie in queue:
+            printi(f"\t{movie['name']}")
+    else:
+        input_file: io.TextIOWrapper = args.input_file
+        queue.append({ 'name': input_file.name, 'path': os.path.abspath(os.path.join('./media', input_file.name)) })
+        
+    #print(json.dumps(queue, indent=2))
     try:
-        printi(f"Streaming {file_name} to owncast server...")
-        stream_to_owncast(
-            input_file=f"{file_name}",
-            rtmp_server_url='127.0.0.1:1935',
-            stream_key=os.getenv('OWNCAST_STREAMKEY'))
+        for movie in queue:
+            try:
+                printi(f"Streaming {movie['name']} to owncast server...")
+                stream_to_owncast(
+                    input_file=f"{movie['path']}",
+                    rtmp_server_url='192.168.1.113:1935',
+                    stream_key=os.getenv('OWNCAST_STREAMKEY'))
+            except KeyboardInterrupt as ke:
+                printe(f"Stopping stream of {movie['name']}, and streaming next. Please wait 1.5 minutes for next film")
+                time.sleep(60*1.5)
     except KeyboardInterrupt as ke:
         print(f'\n {Style.BRIGHT}[{Fore.LIGHTGREEN_EX}-{Fore.RESET}] Goodbye!')
         exit(1)
